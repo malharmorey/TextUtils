9267e01c9b467fa6fb7da9aaada770593fe65cd9
Malhar <malharmorey1436@gmail.com>
2022-03-04 14:31:47 +0530

    Video 6 completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
+  // This App will be kind of  our app's overview file.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* here we pass props via HTMl attributes */}
+      {/* <Navbar title='TextUtils' aboutText='About-TextUtils' /> */}
+      {/* <Navbar /> */}
+      <Navbar title='TextUtils' />
+    </>
   );
 }
 
