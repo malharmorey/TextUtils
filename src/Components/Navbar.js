5bcea96ba4fc8aba16e2b6c634b01c9c1478a0ef
Malhar <malharmorey1436@gmail.com>
2022-06-29 21:33:37 +0530

    First release

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,54 +3,52 @@ import PropTypes from 'prop-types';
 import '../index.css';
 
 export default function Navbar(props) {
-  return (
-    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-      <div className='container-fluid'>
-        <a className='navbar-brand' href='/'>
-          {props.title}
-        </a>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item'>
-              <a className='nav-link active' aria-current='page' href='/'>
-                Home
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='/'>
-                About
-              </a>
-            </li>
-          </ul>
-          <div className='darkMode'>
-            <p className='d-print-inline-block'>Dark Mode</p>
-            <div class='switch'>
-              <label class='theme-switch' for='checkbox'>
-                <input type='checkbox' id='checkbox' />
-                <div class='slider round'></div>
-              </label>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
+			<div className='container-fluid'>
+				<a className='navbar-brand' href='/'>
+					{props.title}
+				</a>
+				<button
+					className='navbar-toggler'
+					type='button'
+					data-bs-toggle='collapse'
+					data-bs-target='#navbarSupportedContent'
+					aria-controls='navbarSupportedContent'
+					aria-expanded='false'
+					aria-label='Toggle navigation'
+				>
+					<span className='navbar-toggler-icon'></span>
+				</button>
+				<div
+					className='collapse navbar-collapse'
+					id='navbarSupportedContent'
+				>
+					<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+						<li className='nav-item'>
+							<a
+								className='nav-link active'
+								aria-current='page'
+								href='/'
+							>
+								Home
+							</a>
+						</li>
+						{/* <li className='nav-item'>
+							<a className='nav-link' href='/'>
+								About
+							</a>
+						</li> */}
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
 Navbar.propTypes = {
-  title: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
 };
 Navbar.defaultProps = {
-  title: 'Your Title here',
+	title: 'Your Title here',
 };
